f173aa6189018c4165f133af151313ce2127caf7
EzharAnsari <ea199100@gmail.com>
2026-02-28 20:58:22 +0530

    feat: implement base62 encoding service

---
+++ src/services/base62.js
@@ -0,0 +1,15 @@
+const characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+const base = characters.length;
+
+function encode(num) {
+  let shortCode = "";
+
+  while (num > 0) {
+    shortCode = characters[num % base] + shortCode;
+    num = Math.floor(num / base);
+  }
+
+  return shortCode;
+}
+
+module.exports = { encode };
