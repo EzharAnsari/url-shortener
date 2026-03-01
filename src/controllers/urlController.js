17b41ddebb65f5011badffd70908c169f74f1e93
EzharAnsari <ea199100@gmail.com>
2026-03-01 13:24:24 +0530

    feat: added expiration support

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -4,16 +4,24 @@ const redisClient = require("../db/redis");
 
 exports.createShortUrl = async (req, res) => {
   try {
-    const { url } = req.body;
+    const { url, expires_in_days } = req.body;
 
     if (!url) {
       return res.status(400).json({ error: "URL is required" });
     }
 
+    if (!expires_in_days) {
+      return res.status(400).json({ error: "expires in days is required" });
+    }
+
+    const expiresAt = expires_in_days
+      ? new Date(Date.now() + expires_in_days * 24 * 60 * 60 * 1000)
+      : null;
+
     // Insert long URL first
     const result = await pool.query(
-      "INSERT INTO urls (long_url) VALUES ($1) RETURNING id",
-      [url]
+      "INSERT INTO urls (long_url, expires_at) VALUES ($1, $2) RETURNING id",
+      [url, expiresAt]
     );
 
     const id = result.rows[0].id;
@@ -48,12 +56,18 @@ exports.redirectUrl = async (req, res) => {
 
     // 2️⃣ If not found in cache → DB
     const result = await pool.query(
-      "SELECT long_url FROM urls WHERE short_code = $1",
+      "SELECT long_url, expires_at FROM urls WHERE short_code = $1",
       [shortCode]
     );
 
     if (result.rows.length === 0) {
       return res.status(404).json({ error: "URL not found" });
+    }
+
+    const expiresAt = result.rows[0].expires_at;
+
+    if (expiresAt && new Date(expiresAt) < new Date()) {
+      return res.status(410).json({ error: "URL expired" });
     }
 
     const longUrl = result.rows[0].long_url;
